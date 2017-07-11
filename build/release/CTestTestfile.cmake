f684a03d92689fa2b2345f2bde3b48b34b3b0f36
Jack Roehr <jack@sphairo.com>
2017-07-11 13:24:07 -0500

    Fixed compile error
    Reverted to pre-genesis block; removed compile accidentally pushed; fixed compile error in src/CMakeLists.txt

--- build/release/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/jack/Documents/Crimson
-# Build directory: /mnt/c/Users/jack/Documents/Crimson/build/release
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(external)
-SUBDIRS(src)
-SUBDIRS(tests)
--- build/release/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/jack/Documents/Crimson/src
-# Build directory: /mnt/c/Users/jack/Documents/Crimson/build/release/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
